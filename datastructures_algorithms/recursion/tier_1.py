939d414523ecad045852d3dabcc2073ef607c5a1
Gabriel Cruz <gabriel.crzprz45@gmail.com>
2026-09-23 09:52:20 -0400

    ex3 of recursion

--- datastructures_algorithms/recursion/tier_1.py
+++ datastructures_algorithms/recursion/tier_1.py
@@ -3,9 +3,11 @@
 
 def factorial(n):
     # Base case
-    print(n)
     if n == 0:
         return 1
+    elif n <=0:
+        print("(-n)! is undefined")
+        return None
     
     return n * factorial(n - 1)
 
@@ -17,5 +19,25 @@ def sum_of_natural_numbers(n):
     # Base case
     if n == 0:
         return 0
+    elif n <=0:
+        print("The natural numbers set -> 1, 2, 3, ..., N")
+        return None
        
     return n + sum_of_natural_numbers(n - 1)
+
+# 03 Naive power Easy
+# Compute x to the power n the obvious recursive way. What's its time complexity?
+
+def naive_power(x, n):
+    # Base Case (Positive Integers/Natural Numbers)
+    if n == 0:
+        return 1
+    
+    # Negative numbers check
+    if n < 0:
+        print("n is outside the scope of this exercise.")    
+        return None
+    
+    return x * naive_power(x, n - 1)
+
+print(naive_power(2, -3))
